27fbdd4d4410a726a6ab8f528f0b8a6be6aecaa1
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-11 17:48:51 +0330

    Add Panel red color in the page

--- Windouw.java
+++ Windouw.java
@@ -1,7 +1,15 @@
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Windouw {
     public static void main(String[] args){
+
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.red);
+        redPanel.setBounds(0, 0, 600 , 150);
+        
 
         JFrame  frame = new JFrame();
         frame.setVisible(true);
