63542e4eaef61e4598fbc93dea13dc6626b13007
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-11 16:41:22 +0330

    add a red panel

--- Mian.java
+++ Mian.java
@@ -5,16 +5,18 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 public class Mian {
     public static void main(String[] args){
-        
-        // Crear a panal
+
         JPanel redPanel = new JPanel();
         redPanel.setBackground(Color.red);
-        redPanel.setBounds(0, 0, 250, 250 );
+        redPanel.setBounds(0 , 0, 250, 250);
+        
+        
           JFrame frame = new JFrame();
           frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
           frame.setLayout(null);
           frame.setSize(750, 750);
           frame.setVisible(true);
+          frame.add(redPanel);
           
       
         
