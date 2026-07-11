4cffc29b14891acd9666c0f78cfc520a7178a76a
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-11 16:38:42 +0330

    add a new large Jframe

--- Mian.java
+++ Mian.java
@@ -10,19 +10,13 @@ public class Mian {
         JPanel redPanel = new JPanel();
         redPanel.setBackground(Color.red);
         redPanel.setBounds(0, 0, 250, 250 );
- 
-        // Create a frame
-        JFrame myFrame = new JFrame();
-        ImageIcon image = new ImageIcon("Pic.jpg");
-        myFrame.setIconImage(image.getImage());
-        // add title the visible 
-        myFrame.setTitle("Java first Swing");
-        // set size of visible
-        myFrame.setSize(420, 420);
-        // make a frame visible 
-        myFrame.setVisible(true);
-        myFrame.getContentPane().setBackground(Color.blue);
-        myFrame.add(redPanel);
+          JFrame frame = new JFrame();
+          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          frame.setLayout(null);
+          frame.setSize(750, 750);
+          frame.setVisible(true);
+          
+      
         
     }
     
