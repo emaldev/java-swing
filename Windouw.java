b45fc8509cb97669247a628fa443f4bead370a11
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-11 17:53:53 +0330

    Adding multiple colors to the page

--- Windouw.java
+++ Windouw.java
@@ -5,15 +5,29 @@ import javax.swing.JPanel;
 
 public class Windouw {
     public static void main(String[] args){
-
+         //Create a red panel
         JPanel redPanel = new JPanel();
         redPanel.setBackground(Color.red);
         redPanel.setBounds(0, 0, 600 , 150);
-        
+
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.blue);
+        bluePanel.setBounds(0, 150, 200, 250);
+
+        JPanel greenPanel  = new JPanel();
+        greenPanel.setBackground(Color.green);
+        greenPanel.setBounds(200, 150, 200, 250);
+
+
+
 
         JFrame  frame = new JFrame();
         frame.setVisible(true);
         frame.setSize(600, 600);
+        frame.add(redPanel);
+        frame.add(bluePanel);
+        frame.add(greenPanel);
+
         
 
 
