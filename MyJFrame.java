a01215325acc9d1686b210a3aa7ef268ac9d4200
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-12 06:47:36 +0330

    Adding 2 Btutoom Firtst buttom print x, and second buttom print 0

--- MyJFrame.java
+++ MyJFrame.java
@@ -1,17 +1,31 @@
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MyJFrame extends JFrame{
     MyJFrame(){
-        JButton button = new JButton();
-        button.setBounds(50, 50, 70, 50);
+        JButton button1 = new JButton();
+        button1.setBounds(0,0 , 80, 50);
+        button1.addActionListener(e -> System.out.println("X"));
+        button1.setText("Button 1.");
+        button1.setBackground(Color.CYAN);
+        button1.setBorder(BorderFactory.createEtchedBorder());
+        JButton button2 = new JButton();
+        button2.addActionListener(e -> System.out.println("0"));
+        button2.setBounds(150, 0, 80, 50);
+        button2.setText("Button 2.");
+        button2.setBackground(Color.MAGENTA);
+        button2.setBorder(BorderFactory.createEtchedBorder());
 
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setVisible(true);
-        this.setSize(699, 699);
-        this.setBounds(0, 0, 250, 250);
+        this.setSize(700, 700);
+        this.setBounds(50, 50, 250, 250);
         this.setLayout(null);
-        this.add(button);
+        this.add(button1);
+        this.add(button2);
     }
     
 }
